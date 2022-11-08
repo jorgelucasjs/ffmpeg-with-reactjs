df1e147ce31a14856439794edb2358a1876e19cd
Jorge LucasJs <jorge.tomasgt@gmail.com>
2022-11-08 18:12:40 +0100

    hls

--- src/App.tsx
+++ src/App.tsx
@@ -59,11 +59,12 @@ function App() {
 			"-sc_threshold", "0", "-segment_time", "7", "-segment_list","playlist.m3u8",
 			"-segment_format","mpegts","file%d.m4a");
 
-			const data = ffmpeg.FS('readFile', 'song.mp3');
-			const music = new Blob([data.buffer], {type: 'audio/mp3'});
+			//ffmpeg -y -i Lazy_Days_320_preview.mp3 -c:a aac -b:a 128k -muxdelay 0 -f segment -sc_threshold 0 -segment_time 7 -segment_list "playlist.m3u8" -segment_format -hls_list_size "file%d.m4a"
 
-			console.log(music);
-		
+			const data = ffmpeg.FS('readFile', 'song.mp3');
+
+			const audio = URL.createObjectURL(new Blob([data.buffer], {type: 'audio/mp3'}));
+			setVideoSrc(audio);
 		}).catch((error)=>{
 			console.log('ERRO ====> ', error);
 		});
@@ -72,7 +73,7 @@ function App() {
 	return (
 		<div className="App">
 			<div>
-				<video height={300} width={600} src={videoSrc} controls></video>
+				<audio src={videoSrc} controls></audio>
 			</div>
 			<div>
 				<legend>Imagem</legend>
